4562ee4228c43056612783614860b27062583020
alyson-singleton <55365254+alyson-singleton@users.noreply.github.com>
2024-07-23 17:13:41 -0700

    clean processing species data file

--- code/01_preprocessing_species_data.R
+++ code/01_preprocessing_species_data.R
@@ -1,7 +1,9 @@
 #------------------------------------------------------
 #------------------------------------------------------
-### Prepare rds data for input into ML functions. Could be changed into functions later on. 
-#       Might need to be project specific.
+### Script used to create preprocessed_datasets above. 
+# Posted for reproducibility. Recommend to just use the 
+# preprocessed_datasets in the data folder, rather than 
+# recreate from scratch.
 #------------------------------------------------------
 #------------------------------------------------------
 
@@ -17,34 +19,10 @@ pacman::p_load(tidyverse, dplyr, kableExtra, knitr, sf, raster, terra, spData,
                gridExtra, RColorBrewer, randomForest, caret, caTools, pdp,
                tidyr, dismo, dismotools, sp, enmSdm, grpregOverlap, stats,readr)
 
-#------------------------------------------------------
-# CHOICE 1: Which occurrence data to use? Either run Expert section *OR* GBIF section.
-#           This is also where you could easily create a combo Expert / GBIF dataset.
-#------------------------------------------------------
-
-# EXPERT SNAIL DATA
 # Load RDS file
-#environmental_covariate_df <- readRDS(file = "~/Desktop/doctorate/ch1 brazil schisto/multi-scale-sdm-schisto/final_data/clean_data/bra_fio_all_snails_all_covariates.rds")
-environmental_covariate_df <- readRDS(file = "~/Desktop/doctorate/ch1 brazil schisto/multi-scale-sdm-schisto/final_data/clean_data/all_expert_all_gbif_data_dec12.rds")
-
-# Quick thinning test
-gbif.test.glab <- read.table("~/Downloads/0244816-230224095556074.csv", sep="\t", header=TRUE)
-gbif.test.stram <- read.table("~/Downloads/0244824-230224095556074.csv", sep="\t", header=TRUE)
-
-gbif.test <- gbif.test.stram
-gbif.test <- as.data.frame(gbif.test)
-gbif.test[,22] <- as.numeric(gbif.test[,22])
-gbif.test[,23] <- as.numeric(gbif.test[,23])
-s <- gridSample(gbif.test[c(23,22)], rast, n=1)
-s.thin <- gbif.test[row.names(s),]
-thin.occ <- occ.target[row.names(s),]; thin.occ <- thin.occ[complete.cases(thin.occ), ]
-
+environmental_covariate_df <- readRDS(file = "../data/full_dataset.rds")
 
 # Only keep variables of interest
-# caroline's fav list: 'bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec'
-# best convergence for small models: 'bio2_diurn', 'bio4_temp_', 'bio5_temp_', 'bio13_prec'
-# mixture: 'bio2_diurn', 'bio4_temp_', 'bio11_temp', 'bio17_prec'
-
 variable_list <- c('bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec',
                    'ag_mosiac', 'distance_high_pop',
                    'hnd', 'soilWater', 'pH', 'clay',
@@ -54,25 +32,20 @@ environmental_covariate_df <- environmental_covariate_df[,variable_list]
 # Catch all to remove any incomplete rows
 environmental_covariate_df <- environmental_covariate_df[complete.cases(environmental_covariate_df), ]
 environmental_covariate_df_stor <- environmental_covariate_df
-saveRDS(environmental_covariate_df, "~/Desktop/full_dataset.rds")
-#------------------------------------------------------
-
-# GBIF v Expert SNAIL DATA
-
-# # Pick GBIF or expert for occurrence data counting tests
-environmental_covariate_df <- environmental_covariate_df %>%
-  filter(source == "expert") #expert, gbif
 
 #------------------------------------------------------
-# CHOICE 2: What is our geographic extent of interest? Only run the six lines that correspond to
-#           the region you want the model to run on. Store as "geo_extent" which is what will be
-#           referenced in the rest of the code. Also store appropriate subregion geometry set to 
-#           use later. Brazil shapefiles pulled from Brazilian govern website. Perhaps worth 
-#           putting on GitHub.
+# CHOICE 1: Which occurrence data to use? All? Expert only? GBIF only?
 #------------------------------------------------------
 
-# Set multiplier
-multiplier <- 2
+# Pick GBIF or expert if want to seperate, otherwise leave commented out
+# environmental_covariate_df <- environmental_covariate_df %>%
+#   filter(source == "expert") #options: expert, gbif
+
+#------------------------------------------------------
+# CHOICE 2: Choose the geographic extent of interest.  Store as "geo_extent" which is what will be
+#           referenced throughout the rest of the file. Also store appropriate subregion geometry set to 
+#           use later. Brazil shapefiles pulled from Brazilian govern website.
+#------------------------------------------------------
 
 ### National
 geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_Pais_2021/BR_Pais_2021.shp")
@@ -82,37 +55,29 @@ subregions$geometry <- st_transform(subregions$geometry, 4326)
 geo_extent <- st_as_sf(geo_extent_shp) 
 geo_extent$x <- st_transform(geo_extent$x, 4326)
 
-### Regional (Sudeste)
-geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_UF_2021/BR_UF_2021.shp")
-subregions <- geo_extent_shp[which(geo_extent_shp$NM_REGIAO=="Sudeste"),] #state as subregion
-subregions$geometry <- st_transform(subregions$geometry, 4326)
-geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$NM_REGIAO=="Sudeste"),])
-geo_extent <- st_as_sf(geo_extent_shp) 
-geo_extent$x <- st_transform(geo_extent$x, 4326)
-
-### State (Minas Gerais)
-geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_Mesorregioes_2021/BR_Mesorregioes_2021.shp")
-subregions <- geo_extent_shp[which(geo_extent_shp$SIGLA=="MG"),] #mesoregion as subregion
-subregions$geometry <- st_transform(subregions$geometry, 4326)
-geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$SIGLA=="MG"),])
-geo_extent <- st_as_sf(geo_extent_shp) 
-geo_extent$x <- st_transform(geo_extent$x, 4326)
-
-### State (Sao Paulo)
-geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_Mesorregioes_2021/BR_Mesorregioes_2021.shp")
-subregions <- geo_extent_shp[which(geo_extent_shp$SIGLA=="SP"),] #mesoregion as subregion
-subregions$geometry <- st_transform(subregions$geometry, 4326)
-geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$SIGLA=="SP"),])
-geo_extent <- st_as_sf(geo_extent_shp) 
-geo_extent$x <- st_transform(geo_extent$x, 4326)
-
-### State (Pernambuco)
-geo_extent_shp <- read_sf("~/Desktop/doctorate/mordecai_lab/brazil schisto/brazil_shapefiles/BR_Mesorregioes_2021/BR_Mesorregioes_2021.shp")
-subregions <- geo_extent_shp[which(geo_extent_shp$SIGLA=="PE"),] #mesoregion as subregion
-subregions$geometry <- st_transform(subregions$geometry, 4326)
-geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$SIGLA=="PE"),])
-geo_extent <- st_as_sf(geo_extent_shp)
-geo_extent$x <- st_transform(geo_extent$x, 4326)
+# ### Regional (Sudeste)
+# geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_UF_2021/BR_UF_2021.shp")
+# subregions <- geo_extent_shp[which(geo_extent_shp$NM_REGIAO=="Sudeste"),] #state as subregion
+# subregions$geometry <- st_transform(subregions$geometry, 4326)
+# geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$NM_REGIAO=="Sudeste"),])
+# geo_extent <- st_as_sf(geo_extent_shp) 
+# geo_extent$x <- st_transform(geo_extent$x, 4326)
+# 
+# ### State (Minas Gerais)
+# geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_Mesorregioes_2021/BR_Mesorregioes_2021.shp")
+# subregions <- geo_extent_shp[which(geo_extent_shp$SIGLA=="MG"),] #mesoregion as subregion
+# subregions$geometry <- st_transform(subregions$geometry, 4326)
+# geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$SIGLA=="MG"),])
+# geo_extent <- st_as_sf(geo_extent_shp) 
+# geo_extent$x <- st_transform(geo_extent$x, 4326)
+# 
+# ### State (Sao Paulo)
+# geo_extent_shp <- read_sf("~/Desktop/doctorate/ch1 brazil schisto/brazil_shapefiles/BR_Mesorregioes_2021/BR_Mesorregioes_2021.shp")
+# subregions <- geo_extent_shp[which(geo_extent_shp$SIGLA=="SP"),] #mesoregion as subregion
+# subregions$geometry <- st_transform(subregions$geometry, 4326)
+# geo_extent_shp <- st_union(geo_extent_shp[which(geo_extent_shp$SIGLA=="SP"),])
+# geo_extent <- st_as_sf(geo_extent_shp) 
+# geo_extent$x <- st_transform(geo_extent$x, 4326)
 
 #------------------------------------------------------
 # CHOICE 3: Choose target species of interest.
@@ -130,10 +95,6 @@ target_species_vector <- c('tenagophila', 'Biomphalaria tenagophila', 'B. tenago
 occ.target <- environmental_covariate_df %>%
   filter(species %in% target_species_vector)
 
-#Expert v GBIF
-# occ.target <- occ.target %>%
-#   filter(source == "expert") #expert, gbif
-
 # Clean lat long columns for thinning procedure
 occ_target_lat_lon <- as.data.frame(occ.target[,c("species", "longitude", "latitude")])
 occ_target_lat_lon$latitude <- as.numeric(occ_target_lat_lon$latitude)
@@ -141,6 +102,7 @@ occ_target_lat_lon$longitude <- as.numeric(occ_target_lat_lon$longitude)
 
 # Load raster to use for grid cell references
 rast <- raster("~/Desktop/doctorate/ch1 brazil schisto/schisto_archives/regional_rasters/national_mean_EVI.tif")
+
 # One point per grid cell
 s <- gridSample(occ_target_lat_lon[2:3], rast, n=1)
 thin.occ <- occ.target[row.names(s),]; thin.occ <- thin.occ[complete.cases(thin.occ), ]
@@ -156,67 +118,18 @@ thin.occ <- occ_sf_points_region
 # Add subregion column
 thin.occ.subregion <- st_intersects(thin.occ$geometry, subregions$geometry, sparse = FALSE)
 thin.occ$subregion <- apply(thin.occ.subregion, 1, which)
-#thin.occ <- st_set_geometry(thin.occ, NULL) #keep geomegry for spatial cv function
 dim(thin.occ) #check number of occurrence points in region of interest
 
 #------------------------------------------------------
-#------------------------------------------------------
-# INTERLUDE INVESTIGATION
-# plot species data on the map
-e <- raster("~/Desktop/brazil_schisto_raster_folders/sp/sp_hnd.tif")
-plot(e)#, xlim=c(-53,-38), ylim=c(-25,-12)) # plot raster data
-plot(thin.occ, 
-     pch = 16, col=alpha("blue",0.2), 
-     add=TRUE) # add absence points
-#plot(analysis_data[which(analysis_data$presence==1), ], pch = 16, col= alpha("red",0.2), add=TRUE) # add presence points
-
-# spatial blocking by specified range with random assignment
-set.seed(5)
-thin.occ.blocking <- thin.occ
-sb2 <- blockCV::spatialBlock(speciesData = thin.occ.blocking,
-                             species = "source",
-                             rasterLayer = e,
-                             rows = 5,
-                             cols = 5,
-                             k = 9)
-                             # selection = "systematic",
-                             # biomod2Format = TRUE)
-thin.occ.blocking$spatialCV_fold <- sb2$foldID
-thin.occ.blocking.large.fold <- thin.occ.blocking[which(thin.occ.blocking$spatialCV_fold==5),]
-set.seed(106)
-rows <- sample(nrow(thin.occ.blocking.large.fold),78)
-thin.occ.blocking.sampled.fold <- thin.occ.blocking.large.fold[rows,]
-thin.occ <- rbind(thin.occ.blocking[which(thin.occ.blocking$spatialCV_fold!=5),],thin.occ.blocking.sampled.fold)
-thin.occ <- subset(thin.occ, select=-c(spatialCV_fold))
-#------------------------------------------------------
-#------------------------------------------------------
-
-# # IF NEED TO LIMIT OCC DATA FOR PROPER OCC/BG RATIO DO HERE
-# # keep "best?" function of predictor variables?
-set.seed(115) #100, 110, 11, 5 (4), 50, 4, 10, 12, 13, 20, 200, 27, 33, 55, 66, 77, 88, 99
-rows <- sample(nrow(thin.occ), 115) #115 #134 #218
-thin.occ <- thin.occ[rows,]
-dim(thin.occ)
-
-plot(e)#, xlim=c(-53,-38), ylim=c(-25,-12)) # plot raster data
-plot(thin.occ, 
-     pch = 16, col=alpha("blue",0.2), 
-     add=TRUE) #
-
-# Build extrapolation occ dataset (anything not in region of interest post thinning, could consider not thinning for this set?)
-#     NOTE: Depending on the comparison we want to make, we could have it be those outside the region *and* inside,
-#     to compare national predicting national to minas gerais predicting national (not minas gerais predicting national-minas gerais?)
-extrapolation.occ <- occ_sf_points[apply(occ_by_region,1,isFALSE),]
-#for now do not build subregion column for extrapolation points
-#extrapolation.occ.subregion <- st_intersects(extrapolation.occ$geometry, subregions$geometry, sparse = FALSE)
-#extrapolation.occ$subregion <- apply(extrapolation.occ.subregion, 1, which)
-#extrapolation.occ <- st_set_geometry(extrapolation.occ, NULL) #keep geomegry for spatial cv function
-dim(extrapolation.occ) #check number of occurrence points outside region of interest
+# If need to limit no. of occurrences for Figure E in S1 text uncomment next four lines:
+# set.seed(115) 
+# rows <- sample(nrow(thin.occ), 115)
+# thin.occ <- thin.occ[rows,]
+# dim(thin.occ)
 
 #------------------------------------------------------
 # Build background species / background mask rows
 #------------------------------------------------------
-
 # Limit to background species (non target species, non wo species specification)
 bg_df <- environmental_covariate_df_stor %>%
   filter(!species %in% c(target_species_vector, '', 'sp.'))
@@ -225,7 +138,7 @@ bg_df <- environmental_covariate_df_stor %>%
 rast <- raster("~/Desktop/doctorate/ch1 brazil schisto/schisto_archives/regional_rasters/national_mean_EVI.tif")
 bg_species_list <- unique(bg_df$species)
 
-# Extract number of Insecta (+ supplemental) background points per grid cell (i.e., weighted bias mask)
+# Extract number of possible background points per grid cell (i.e., weighted bias mask)
 bg_points <- bg_df %>% dplyr::select(c(longitude, latitude)) %>%
   as.matrix()
 
@@ -261,7 +174,9 @@ bg_sf_points_region$subregion <- apply(bg_mask_sf_subregion, 1, which)
 bg_mask_sf <- bg_sf_points_region
 dim(bg_mask_sf) #check number of background points in region of interest
 
-# Random sample bg without replacement from weighted bias mask at (1.5/2x occ) multiplier
+# Random sample bg without replacement from weighted bias mask at 2x occ multiplier
+multiplier <- 2
+
 set.seed(9)
 
 bg_mask_weights <- bg_mask_sf %>%
@@ -276,28 +191,6 @@ bg_mask_df <- bg_mask_sf[sample(nrow(bg_mask_weights),
 bg_df_wcovariates <- cbind(bg_df[c(bg_mask_df$max_index),], bg_mask_df$geometry, bg_mask_df$subregion)
 colnames(bg_df_wcovariates)[dim(bg_df_wcovariates)[2]] <- 'subregion'
 
-# Build extrapolation bg dataset (anything not in region of interest post thinning, could consider not thinning for this set?)
-#       Same discussion points as above. Would also need to change here.
-extrapolation.bg <- bg_mask_sf_full[apply(bg_by_region,1,isFALSE),]
-dim(extrapolation.bg) #check number of background points outside region of interest
-# Don't make subregion for extrapolation datasets yet
-#extrapolation.bg.subregion <- st_intersects(extrapolation.bg$geometry, subregions$geometry, sparse = FALSE)
-#extrapolation.bg$subregion <- apply(extrapolation.bg.subregion, 1, which)
-
-# Random sample bg without replacement from weighted bias mask at (1.5/2x occ) multiplier
-extrapolation.bg.weights <- extrapolation.bg %>%
-  mutate(weight = count/sum(count))
-
-extrapolation.bg <- extrapolation.bg[sample(nrow(extrapolation.bg.weights),
-                                size = multiplier * nrow(extrapolation.occ),
-                                replace = FALSE,
-                                prob = extrapolation.bg.weights$weight),]
-
-#extrapolation.bg <- st_set_geometry(extrapolation.bg, NULL)#keep geometry for spatial cv function
-extrapolation.bg <- bg_df[c(extrapolation.bg$max_index),]
-#question.. do we want to ask it to predict all potential background points or just the multiplier * 
-#       the extrapolated occurrences? for now have it set as the latter to be as comparable as possible
-
 #------------------------------------------------------
 # Build final input "all_data" set
 #------------------------------------------------------
@@ -305,53 +198,24 @@ extrapolation.bg <- bg_df[c(extrapolation.bg$max_index),]
 #Build outcome row and only retain predictor columns
 thin.occ$presence <- rep(1, nrow(thin.occ))
 thin.occ <- data.frame(thin.occ)
-#thin.occ <- subset(thin.occ, select=-c(species, year, source))
 thin.occ <- thin.occ[,c('presence','bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec',
                         'ag_mosiac', 'distance_high_pop', 'hnd', 'soilWater', 'pH', 'clay',
-                        'geometry', 'subregion')]#, 'source', 'species')]
+                        'geometry', 'subregion')]
 
 bg_df_wcovariates$presence <- rep(0, nrow(bg_df_wcovariates))
 bg_df_wcovariates <- data.frame(bg_df_wcovariates)
-#bg_df_wcovariates <- subset(bg_df_wcovariates, select=-c(species, year, source, index))
 bg_df_wcovariates <- bg_df_wcovariates[,c('presence','bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec',
                                           'ag_mosiac', 'distance_high_pop', 'hnd', 'soilWater', 'pH', 'clay',
-                                          'geometry', 'subregion')]#, 'source', 'species')]
+                                          'geometry', 'subregion')]
 all_data <- rbind(thin.occ, bg_df_wcovariates)
+
 # Add subregion effects as categorical variable of numbers
 all_data$subregion <- as.factor(all_data$subregion)
 
-#sanity check
+# Sanity check
 dim(all_data)
 table(all_data$presence)
 names(all_data)
 
+# Save file
 saveRDS(all_data, "~/Desktop/doctorate/ch1 brazil schisto/late_feb_runs/datasets/tenagophila_sp_limited.rds")
-saveRDS(test, "~/Desktop/schisto_manuscript_figs/jan_mlk_runs/sp_occurences_thinned_mapping_stram_tenag.rds")
-
-test <- readRDS("~/Desktop/schisto_manuscript_figs/jan_mlk_runs/sp_occurences_thinned_mapping.rds")
-#test2 <- readRDS("~/Desktop/schisto_manuscript_figs/jan_mlk_runs/mg_straminea_occurences_thinned_mapping.rds")
-test <- test[,c(1,12:15)]
-thin.occ <- thin.occ[,c(1,12:15)]
-test <- rbind(test, thin.occ)
-test <- test[which(test$species!="B. straminea"),]
-thin.occ[which(thin.occ$species=="glabrata"),5] <- c("B. glabrata")
-
-#------------------------------------------------------
-# Build extrapolation dataset for subregional model tests
-#------------------------------------------------------
-
-# extrapolation.occ$presence <- rep(1, nrow(extrapolation.occ))
-# extrapolation.occ <- extrapolation.occ[,c('presence','bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec', 'hnd',
-#                         'total_population', 'clay', 'forest', 'fresh_water', 'pasture', 'agriculture')]
-# extrapolation.bg$presence <- rep(0, nrow(extrapolation.bg))
-# extrapolation.bg <- extrapolation.bg[,c('presence','bio4_temp_', 'bio11_temp', 'bio16_prec', 'bio17_prec', 'hnd',
-#                   'total_population', 'clay', 'forest', 'fresh_water', 'pasture', 'agriculture')]
-# extrapolation_data <- rbind(extrapolation.occ, extrapolation.bg)
-# # Add subregion effects as categorical variable of numbers (at the moment not in use for extrapolation dataset)
-# #extrapolation_data$subregion <- as.factor(extrapolation_data$subregion)
-
-test <- readRDS("~/Desktop/doctorate/ch1 brazil schisto/late_feb_runs/datasets/tenagophila_sp_expert_limited.rds")
-head(test)
-test <- test[,c(1:13)]
-head(test)
-saveRDS(test, "~/Desktop/doctorate/ch1 brazil schisto/late_feb_runs/datasets/tenagophila_sp_gbif.rds")
